c01d3e9b24498f068cce77bda2d610cce727ba4b
sherylsabol <sherylsabol@yahoo.com>
2017-03-08 11:27:28 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+angular.module('bestArticles', ['ui.router'])
+
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+
+    })
+
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+
+
+  $urlRouterProvider.otherwise('home');
+}])
+
+
+.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}])
+
+.controller('MainCtrl', [
+'$scope',
+'posts',
+
+function($scope, posts){
+  $scope.posts = posts.posts;
+
+  $scope.addPost = function(){
+    if(!$scope.title || $scope.title === '') { return; }
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0,
+      comments: [
+        // {author: 'Dave', body: 'Great article!', upvotes: 0},
+        // {author: 'Mary', body: 'Thanks for sharing!', upvotes: 0}
+      ]
+      });
+    $scope.title = '';
+    $scope.link = '';
+  }
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  }
+}])
+
+.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts) {
+  $scope.post = posts.posts[$stateParams.id];
+
+  $scope.addComment = function() {
+    if ($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    })
+
+  $scope.body = '';
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  }
+}
+}])
